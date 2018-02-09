1dda7fbccfdccfbb0d3053a9088235dd78494943
HugoNF <32874545+HugoNF@users.noreply.github.com>
2018-02-09 15:11:22 +0100

    PHP eval partie 2 = convertisseur eur-dollar

---
+++ index.php
@@ -0,0 +1,20 @@
+<h3>convertisseur d'euros en dollar</h3>
+<p>Taux de change actuel : 1EUR -> 1,22574USD</p>
+<form action="#" method="POST" name="form1">
+    <input type="text" name="valeur" id="valeur">
+    <input type="submit" name="validate" value="Envoyer">
+</form>
+<?php
+
+    if(isset($_POST['validate'])) {
+
+        $valeur = $_POST['valeur']*1.22574;
+
+
+        echo $valeur . ' ' . 'Dollar américain';
+    }
+else{
+    echo 'Veuillez entrer un nombre';
+}
+
+?>
